b4edf13357a45941d2cf4ce5a8d30e4e14025388
dtinth on MBP M1 <dtinth@spacet.me>
2021-08-27 18:46:29 +0700

    Extract repository variable

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,8 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('automessage-codex.suggestCommitMessage', async () => {
 		const gitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git')!.exports;
 		const git = gitExtension.getAPI(1);
-		const diff = await git.repositories[0].diff(true);
+		const repository = git.repositories[0];
+		const diff = await repository.diff(true);
 		vscode.window.showInformationMessage(diff);
 	});
 
