4becb28edd2ae10cc09aea5a7273a61621a6af32
dtinth on MBP M1 <dtinth@spacet.me>
2021-08-27 19:50:52 +0700

    Suggest commit messages in a QuickPick

--- src/extension.ts
+++ src/extension.ts
@@ -35,35 +35,42 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     'automessage-codex.suggestCommitMessage',
     async () => {
-      const apiKey = await getApiKey()
-      if (!apiKey) {
-        return
-      }
-
-      const gitExtension =
-        vscode.extensions.getExtension<GitExtension>('vscode.git')!.exports
-      const git = gitExtension.getAPI(1)
-      const repository = git.repositories[0]
-      const diff = await repository.diff(true)
-
-      const prompt = [
-        '$ git add --all',
-        '',
-        '$ git diff --cached',
-        diff,
-        '',
-        "$ git diff --cached -U0 | grep '^[+-]'",
-        diff
-          .split('\n')
-          .filter((line) => line.startsWith('+') || line.startsWith('-'))
-          .join('\n'),
-        '',
-        '$ git commit -m "',
-      ].join('\n')
-      outputChannel.appendLine('# Prompt')
-      outputChannel.appendLine(prompt)
-
       try {
+        const apiKey = await getApiKey()
+        if (!apiKey) {
+          return
+        }
+
+        const gitExtension =
+          vscode.extensions.getExtension<GitExtension>('vscode.git')!.exports
+        const git = gitExtension.getAPI(1)
+        const repository = git.repositories[0]
+        const diff = await repository.diff(true)
+
+        const prompt = [
+          '$ git add --all',
+          '',
+          '$ git diff --cached',
+          diff,
+          '',
+          "$ git diff --cached -U0 | grep '^[+-]'",
+          diff
+            .split('\n')
+            .filter((line) => line.startsWith('+') || line.startsWith('-'))
+            .join('\n'),
+          '',
+          '$ git commit -m "',
+        ].join('\n')
+        outputChannel.appendLine('# Prompt')
+        outputChannel.appendLine(prompt)
+
+        const quickPick = vscode.window.createQuickPick()
+        quickPick.onDidAccept(() => {
+          const selected = quickPick.selectedItems[0]
+          repository.inputBox.value = selected.label
+        })
+        quickPick.show()
+
         const response = await axios.post(
           'https://api.openai.com/v1/engines/davinci-codex/completions',
           {
@@ -84,15 +91,24 @@ export function activate(context: vscode.ExtensionContext) {
         )
         outputChannel.appendLine('# Response')
         outputChannel.appendLine(JSON.stringify(response.data, null, 2))
+        quickPick.items = createQuickPickItems(response.data.choices)
       } catch (error: any) {
         vscode.window.showErrorMessage(
-          'Unable to get suggestions: ' + error.message,
+          'Unable to suggest commit message: ' + error.message,
         )
         outputChannel.appendLine('# Error')
         outputChannel.appendLine(error.stack)
       }
     },
   )
+
+  function createQuickPickItems(
+    choices: { text: string }[],
+  ): vscode.QuickPickItem[] {
+    return choices.map((choice) => ({
+      label: choice.text,
+    }))
+  }
 
   context.subscriptions.push(disposable)
 }
