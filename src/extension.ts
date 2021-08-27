d82f3c0098913029fae18f6c5c5385c0bd5a2c9d
dtinth on MBP M1 <dtinth@spacet.me>
2021-08-27 18:44:36 +0700

    Show Git diff

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,12 @@
 import * as vscode from 'vscode';
+import { GitExtension } from './vendor/git';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('automessage-codex.suggestCommitMessage', () => {
-		vscode.window.showInformationMessage('Hello World from automessage-codex!');
+	let disposable = vscode.commands.registerCommand('automessage-codex.suggestCommitMessage', async () => {
+		const gitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git')!.exports;
+		const git = gitExtension.getAPI(1);
+		const diff = await git.repositories[0].diff(true);
+		vscode.window.showInformationMessage(diff);
 	});
 
 	context.subscriptions.push(disposable);
